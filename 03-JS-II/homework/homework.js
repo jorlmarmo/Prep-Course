2d93cded927d7cdd183ae71a85ab05f1adfd3db8
jorlmarmo <jormartm02@hotmail.com>
2022-09-25 06:24:35 -0600

    03-JS-II/homework/homework.js

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -57,7 +57,7 @@ function saludo(idioma) {
   else if (idioma === 'ingles') {
     return 'Hello!';
   } else {
-    return 'Hola';
+    return 'Hola!';
   }
 }
 
@@ -161,15 +161,20 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-if (numero < 2) return false;
-if (numero === 2 ) return true;
+if (numero < 2) {
+  return false;
+}
+if (numero === 2 ) {
+  return true;
+}
 for (var i = 2; i < numero; i++) {
     if (numero % i === 0) {
     return false; 
 }
+}
 return true;
 }
-}
+
 
 
 function esVerdadero(valor){
